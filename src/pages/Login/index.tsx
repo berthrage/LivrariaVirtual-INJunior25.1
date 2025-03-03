1de35c8408215d7ddb01755f51e84aab2efbd1db
Mia Berth <miaberthrage@gmail.com>
2025-03-03 19:48:54 -0300

    feat(Login): Validação e Input (zod e react-hook-form)

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -2,9 +2,42 @@ import styles from './styles.module.css'
 import cozyImg from '../../assets/images/cozy-bookstand.jpg'
 import logoLarge from '../../assets/images/Logo-large.png'
 import PrimaryButton from '../../components/PrimaryButton'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
+
+const userSchema = z.object({
+    email: z.string().nonempty('E-mail deve ser inserido.').refine(
+        value => z.string().email().safeParse(value).success, {
+            message: `O e-mail não é válido.`,
+        }),
+
+    password: z.string().nonempty('Senha deve ser inserida.').min(6, 'Senha deve haver pelo menos 6 caracteres.'),
+});
+
+type User = z.infer<typeof userSchema>;
 
 export default function Login() {
+    const { register, handleSubmit, formState: {errors, isSubmitting}, setError } = useForm<User>({
+        resolver: zodResolver(userSchema),
+    });
+
+    const navigate = useNavigate();
+
+    async function EnterUser(data: any) {
+        try {
+            await new Promise(resolve => setTimeout(resolve, 1000));
+            console.log(data);
+            navigate('/');
+        } catch {
+            setError('root', {
+                message: 'Erro entrando em conta.'
+            })
+
+        }
+    }
+
     return(
         <> 
             <section className={styles.mainSection}>
@@ -21,13 +54,35 @@ export default function Login() {
                     </div>
                     <form className={styles.loginForm}>
                         <label>E-mail</label>
-                        <input type="text" placeholder="Digite aqui seu e-mail"></input>
+                        <input 
+                            type="text" 
+                            placeholder="Digite aqui seu e-mail"
+                            {...register('email')}>
+                        </input>
+                        {errors.email && <span className={styles.error}>{errors.email.message}</span>}
+
+
                         <label>Senha</label>
-                        <input type="password" placeholder="Digite aqui sua senha"></input>
+                        <input 
+                            type="password" 
+                            placeholder="Digite aqui sua senha"
+                            {...register('password')}>
+                        </input>
+                        {errors.password && <span className={styles.error}>{errors.password.message}</span>}
+
                     </form>
                     <div className={styles.buttonsSection}> 
-                        <PrimaryButton type="button">Entrar</PrimaryButton>
-                        <PrimaryButton id={styles.signInButton}type="button">Cadastre-se</PrimaryButton>
+                        <PrimaryButton 
+                            disabled={isSubmitting} 
+                            type="button"
+                            onClick={() => handleSubmit(EnterUser)()}>
+                                {isSubmitting ? 'Carregando...' : 'Entrar'}
+                        </PrimaryButton>
+                        <PrimaryButton 
+                            id={styles.signInButton}
+                            type="button">
+                                Cadastre-se
+                        </PrimaryButton>
                     </div>
                 </section>
             </section>
