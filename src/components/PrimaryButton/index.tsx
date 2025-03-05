f0b05e0127c9686519d119c1a0f06c9ca6305341
Berth <miaberthrage@gmail.com>
2025-03-05 00:20:04 -0300

    feat(BookShowcase, PrimaryButton): Responsividade

--- src/components/PrimaryButton/index.tsx
+++ src/components/PrimaryButton/index.tsx
@@ -1,16 +1,28 @@
+import { Link } from 'react-router-dom';
 import styles from './styles.module.css';
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
     children: React.ReactNode;
     className?: string;
+    linkTo?: string;
 }
 
 export default function PrimaryButton(props: ButtonProps) {
     return (
-        <>
-            <button {...props} className={`${styles.button} ${props.className}`}>
-                {props.children}
-            </button>
+        <>  
+            {props.linkTo ? 
+                <Link to={props.linkTo? props.linkTo : ''}>
+                    <div className={styles.buttonContainer}>
+                        <button {...props} className={`${styles.button} ${props.className}`}>
+                            {props.children}
+                        </button>
+                    </div>
+                </Link> : 
+                <div className={styles.buttonContainer}>
+                    <button {...props} className={`${styles.button} ${props.className}`}>
+                        {props.children}
+                    </button>
+                </div>}
         </>
     )
 }
