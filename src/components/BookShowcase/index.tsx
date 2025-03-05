c6537144182274e63d1756672a43bd5a774a8f8e
Berth <miaberthrage@gmail.com>
2025-03-04 22:58:04 -0300

    feat(BookShowcase): Estrutura e Estilização

--- src/components/BookShowcase/index.tsx
+++ src/components/BookShowcase/index.tsx
@@ -3,6 +3,7 @@ import Book from "../../types/Book";
 import { Link } from "react-router-dom";
 import arrow from '../../assets/icons/Arrow.png';
 import arrowHovered from '../../assets/icons/Arrow-hovered.png';
+import PrimaryButton from "../PrimaryButton";
 
 interface BookShowcaseProps {
     book: Book;
@@ -23,23 +24,33 @@ export default function BookShowcase(props: BookShowcaseProps) {
                 </div>
                 </Link>
 
-                <div className={styles.bookCover}>
-                    <img src={props.book.capa} alt="Capa do livro"></img>
-                </div>
-                <div className={styles.bookInfo}>
-                    <div className={styles.titleAuthor}>
-                        <h2
-                            className={
-                                props.book.titulo.length < 28 ? styles.title :
-                                props.book.titulo.length < 40 ? styles.titleSmall : styles.titleSmallest
-                            }
-                        >{props.book.titulo}</h2>
-                        <h3>{props.book.autor}</h3>
+                <div className={styles.book}>
+                    <div className={styles.bookCoverBox}>
+                        <div className={styles.bookCover}>
+                            <img src={props.book.capa} alt="Capa do livro"></img>
+                        </div>
                     </div>
-                    <div className={styles.priceSection}>
-                        <span>R$ {String(props.book.preco.toFixed(2)).replace('.', ',')}</span>
+
+                    <div className={styles.bookInfo}>
+                        <div className={styles.titleAuthor}>
+                            <h2>{props.book.titulo}</h2>
+                            <h3>{props.book.autor}</h3>
+                        </div>
+                        <div className={styles.synopsisSection}>
+                            <h1>Sinopse</h1>
+                            <p>{props.book.sinopse}</p>
+                        </div>
+                        <div className={styles.priceSection}>
+                            <PrimaryButton
+                                id={styles.button}>
+                                <span>R$ {String(props.book.preco.toFixed(2)).replace('.', ',')}</span>
+                                <p>Adicionar ao carrinho</p>
+                            </PrimaryButton>
+                        </div>
                     </div>
                 </div>
+
+                
             </section>
         </>
     )
